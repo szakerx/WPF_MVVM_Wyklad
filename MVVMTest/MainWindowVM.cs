24cd2d947023a3193787aa002e2b7f5f6bb017bd
Szymon <lipieczsp6@gmail.com>
2019-06-12 21:23:18 +0200

    Aplikacja wrzucona po raz pierwszy. Gotowy guzik zmieniający tekst.

---
+++ MVVMTest/MainWindowVM.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace MVVMTest {
+    class MainWindowVM :INotifyPropertyChanged{
+        private string _text;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainWindowVM() {
+            _text = "Text example";
+            ChangeTextButtonCommand = new RelayCommand(ChangeText);
+        }
+        private void OnPropertyChanged(string propName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+        public ICommand ChangeTextButtonCommand { get; set; }
+
+        public string Text {
+            get { return _text; }
+            set { _text = value;
+                OnPropertyChanged("Text");
+            }
+        }
+        private void ChangeText(object obj) {
+            Text = "Changed!";
+        }
+    }
+}
