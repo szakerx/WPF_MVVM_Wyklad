457126bc7480a59b7186e02f8517f8cee4cf7713
Szymon <lipieczsp6@gmail.com>
2019-06-13 09:59:42 +0200

    private bool CanGetUsers to gray the Information button

--- MVVMTest/MainWindowVM.cs
+++ MVVMTest/MainWindowVM.cs
@@ -1,35 +1,60 @@
-﻿
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Windows.Input;
 
 namespace MVVMTest {
-    class MainWindowVM :INotifyPropertyChanged{
-        private string _text;
-
+    class MainWindowVM : INotifyPropertyChanged {
+        #region Private fields
+        private string _welcomeMessage;
+        private List<User> _users;
+        private List<string> _usersList;
+        #endregion
+        #region Events
         public event PropertyChangedEventHandler PropertyChanged;
-
+        #endregion
+        #region Properties
+        public ICommand AddUserCommand { get; private set; }
+        public ICommand GetUsersInformationCommand { get; private set; }
+        public string Name { private get; set; }
+        public string WelcomeMessage {
+            get { return _welcomeMessage; }
+            private set {
+                _welcomeMessage = value;
+                OnPropertyChanged("WelcomeMessage");
+            }
+        }
+        public List<string> UsersList {
+            get { return _usersList; }
+            private set {
+                _usersList = value;
+                OnPropertyChanged("UsersList");
+            }
+        }
+        #endregion
         public MainWindowVM() {
-            _text = "Text example";
-            ChangeTextButtonCommand = new RelayCommand(ChangeText);
-            ChangeTextButtonCommand = new RelayCommand(HelloUser);
+            AddUserCommand = new RelayCommand(HelloUser);
+            GetUsersInformationCommand = new RelayCommand(GetUsersInformation,CanGetUsers);
+            _users = new List<User>();
         }
         private void OnPropertyChanged(string propName) {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
         }
-        public ICommand ChangeTextButtonCommand { get; set; }
 
-        public string Text {
-            get { return _text; }
-            set { _text = value;
-                OnPropertyChanged("Text");
-            }
-        }
-        private void ChangeText(object obj) {
-            Text = "Changed!";
-            Console.WriteLine("dupa");
-        }
         private void HelloUser(object obj) {
+            WelcomeMessage = "Witaj " + Name;
+            _users.Add(new User(Name));
+        }
+        private void GetUsersInformation(object obj) {
+            List<string> temp = new List<string>();
+            foreach (var x in _users)
+                temp.Add(x.Id + " " + x.Name);
+            UsersList = temp;
+        }
+        private bool CanGetUsers(object obj) {
+            if (_users.Count < 1)
+                return false;
+            return true;
         }
     }
 }
---
+++ MVVMTest/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMTest {
+    public class User {
+        public string Name { get; private set; }
+        public int Id { get; private set; }
+        private static int _id = 0;
+        public User(string name) {
+            Name = name;
+            Id = _id;
+            _id++;
+        }
+        public User() {
+            Name = "Default";
+            Id = _id;
+            _id++;
+        }
+    }
+}
